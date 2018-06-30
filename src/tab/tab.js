0bd202398f0e03906352a38010c4e93d8da64f56
Edouard Penin <e.peninb@gmail.com>
2018-06-30 07:50:50 +0200

    placeholder color while loading

--- src/tab/tab.js
+++ src/tab/tab.js
@@ -1,15 +1,8 @@
-console.log( "Wuuuut!" )
+// [ ] - @Todo: Drake head icon
+// [ ] - @Todo: Offline mode
+// [ ] - @Todo: Slow network loading bar
 
-
-// [/] -  Have gifs
-// [/] -  Display one gif
-// [/] -  Display one random gif
-// [ ] -  Always show a different gif
-// [ ] -  Local/cached gifs
-// [ ] -  tell source
-// [ ] -  placeholder jpg / loader
-
-const pictures = [
+const PICTURES = [
 	"https://media.giphy.com/media/5xaOcLDE64VMF4LqqrK/giphy.gif",
 	"https://media.giphy.com/media/l2RnB2zd7hxtNimxa/giphy.gif",
 	"https://media.giphy.com/media/8zT0D36Myf9C0/giphy.gif",
@@ -26,21 +19,78 @@ const pictures = [
 	"https://media.giphy.com/media/xT0xepOGU8SaCzc25O/giphy.gif",
 	"https://media.giphy.com/media/Rspfpv1Rop50k/giphy.gif",
 ]
+const COLORS = [
+	"#9933ff",
+	"#fff35c",
+	"#9933ff",
+	"#01ff98",
+	"#03ccff",
+	"#ff6665",
+]
 
-/**
- * @type {HTMLElement | null}
- */
-const pictureHolder = document.getElementById( "hero" )
-
-pictureHolder.setAttribute( "src", getRandom( pictures ) )
-
-
-/**
- *
- * @param arr {Array}
- * @return {string}
- */
-function getRandom( arr )
+class App
 {
-	return arr[ Math.floor( Math.random() * arr.length ) ]
+	constructor( pictures, colors )
+	{
+		this.__PICTURES = pictures
+		this.__COLORS = colors
+	}
+	
+	
+	init( memeClassName )
+	{
+		this.__memeHolder = document.getElementById( memeClassName )
+		
+		this.__displayMeme()
+	}
+	
+	
+	__displayMeme()
+	{
+		this.__wrapImageWithPlaceholder(this.__memeHolder)
+		
+		this.__memeHolder.classList.add( "js-loading" )
+		
+		this.__tellLoading()
+		
+		this.__memeHolder.onload = _ => this.__tellDoneLoading()
+		
+		this.__memeHolder.src = this.__getRandomFromArr( this.__PICTURES )
+	}
+	
+	__wrapImageWithPlaceholder(img)
+	{
+		const wrapper = document.createElement( "div" )
+		
+		wrapper.style.background = this.__getRandomFromArr( this.__COLORS )
+		
+		img.parentElement.insertBefore( wrapper, img )
+		
+		wrapper.appendChild( img )
+	}
+	
+	__tellLoading()
+	{
+		this.__loading = true
+		document.body.classList.add( "js-loading" )
+	}
+	
+	__tellDoneLoading()
+	{
+		this.__loading = false
+		
+		document.body.classList.remove( "js-loading" )
+		
+		document.body.classList.add( "js-loaded" )
+		
+	}
+	
+	__getRandomFromArr( arr )
+	{
+		return arr[ Math.floor( Math.random() * arr.length ) ]
+	}
 }
+
+const app = new App( PICTURES, COLORS )
+
+app.init( "hero" )
